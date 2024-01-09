950f0ff375513985a7e44ddbc37f7a92d16e73c4
Alex Nguyen <alex.nguyen369@gmail.com>
2024-01-09 08:52:10 -0800

    Added key property for .map()

--- osn-discovery-webui/src/components/BucketList.tsx
+++ osn-discovery-webui/src/components/BucketList.tsx
@@ -1,7 +1,10 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import Bucket from './Bucket'
+import { transformBytes } from "../utils/transformBytes"
 import '../assets/styles/BucketList.css'
+import '../assets/styles/Bucket.css'
 
 interface BucketDetails {
         'bytes-used': number,
@@ -20,8 +23,13 @@ function BucketList() {
 
     // Api call to get buckets and their details
     const getBuckets = async () => {
-        const { data } = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/buckets`)
-        setBucketDetails(data)
+        try {
+            const { data } = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/buckets`)
+            setBucketDetails(data)
+        } catch (error) {
+            console.log(error)
+        }
+        
     }
 
     useEffect(() => {
@@ -36,7 +44,13 @@ function BucketList() {
                 )} */}
                 {/* {!loading && ( */}
                     {Object.keys(bucketDetails).map((key) => (
-                        <Bucket bucketName={bucketDetails[key]['name']} bucketLoc={getLocation(bucketDetails[key]['site'])} bucketNumFiles={bucketDetails[key]['object-count']} bucketSize={bucketDetails[key]['bytes-used']} bucketPath={key} key={key}/>            
+                        <div className='rowItem' key={`${key}`}>
+                            <Link to={`/details/${key}`} className='bucketName' >{bucketDetails[key]['name']}</Link>
+                            <div className='bucketLocation'>{getLocation(bucketDetails[key]['site'])}</div>
+                            <div className='bucketSize'>{transformBytes(bucketDetails[key]['bytes-used'])}</div>
+                            <div className='bucketNumFiles'>{bucketDetails[key]['object-count']}</div>
+                        </div>
+                        // <Bucket bucketName={bucketDetails[key]['name']} bucketLoc={getLocation(bucketDetails[key]['site'])} bucketNumFiles={bucketDetails[key]['object-count']} bucketSize={bucketDetails[key]['bytes-used']} bucketPath={key} key={key}/>            
                     ))}
                 {/* )} */}
         </>
