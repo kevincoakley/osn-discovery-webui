a54117b18a7e66f737ddc10183380ac7d28eeba1
Alex Nguyen <atn012@ucsd.edu>
2024-02-20 05:27:19 -0800

    Add logos to bottom bar
    OSN and NSDF logos added to right side of the bottom bar. Should be responsive
    to changes in viewport

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -1,4 +1,6 @@
 import '../assets/styles/Nav.css'
+import OSNLogo from '/img/OSN_Logo.png'
+import NSDFLogo from '/img/NSDF_Logo.png'
 import { NavLink } from "react-router-dom"
 function BottomBar() {
     return (
@@ -8,6 +10,10 @@ function BottomBar() {
                     {/* TODO: Navigate to actual About and Contact Us Pages */}
                     <NavLink to="/home">About</NavLink>
                     <NavLink to="/home">Contact Us</NavLink>
+                </div>
+                <div id="Logos">
+                    <img src={OSNLogo} alt="Open Storage Network Logo"/>
+                    <img src={NSDFLogo}  alt="National Science Data Fabric Logo"/>
                 </div>
             </nav>
             
